8b47fb3f236702f361b22574352b8890af04df5a
ShubhamDhimanDev <dhiman007shubham@gmail.com>
2026-01-20 15:47:39 +0530

    payU fixed

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,11 +6,12 @@ use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use App\Models\Payment;
 
 class Booking extends Model
 {
-  use HasFactory;
+  use HasFactory, SoftDeletes;
 
   protected $guarded = [];
 
